90792b2cffd4e039c5c28432ce4fdd74208f6bad
ernisol <erwan.nisol@free.fr>
2023-09-10 13:22:49 +0200

    setup.py

---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name="{{cookiecutter.project_name}}",
+    version="{{cookiecutter.version}}",
+    description="{{cookiecutter.project_short_description}}",
+    author="{{cookiecutter.full_name}}",
+    author_email="{{cookiecutter.email}}",
+)
